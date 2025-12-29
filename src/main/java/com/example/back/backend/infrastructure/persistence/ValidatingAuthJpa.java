7e8fc807c51ed386eff312c94bf3ae4008f1b710
FrydoIW <sologankerfrydo99@gmail.com>
2025-12-29 21:59:25 +0700

    Adding new user_master condition

--- src/main/java/com/example/back/backend/infrastructure/persistence/ValidatingAuthJpa.java
+++ src/main/java/com/example/back/backend/infrastructure/persistence/ValidatingAuthJpa.java
@@ -22,6 +22,7 @@ public class ValidatingAuthJpa {
                 WHERE A.EMAIL = :email
                   AND B.PASSWORD_HASH = :password
                   AND B.STATUS = '00'
+                  AND A.USER_MASTER = 'TRUE'
             )
         """;
 
